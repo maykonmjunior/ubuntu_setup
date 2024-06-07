ed623668bde7c68241f7985ff31a907ec913fe45
maykonmarcosjunior <maykon.marcos.junior@grad.ufsc.br>
2024-06-07 12:11:11 -0300

    bridge é só para planos pagos...

--- src/shs/proton_mail.sh
+++ src/shs/proton_mail.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
 # Nome do repositório no formato usuario/repositorio
-REPO="ProtonMail/proton-bridge"
+REPO="vladimiry/ElectronMail"
 # pasta home do usuário
-HOME_NAME="$(whoami)"
+HOME_NAME="maykon-marcos-junior"
 # extensão do arquivo
-EXTENSION="deb"
+EXENSION="deb"
 # nome do arquivo
-FILE_NAME="proton-bridge"
+FILE_NAME="electron-mail"
 # Caminho temporário absoluto para salvar o .deb
-TEMP_DEB_PATH="/home/$HOME_NAME/Downloads/$EXTENSION/$FILE_NAME.$EXTENSION"
+TEMP_DEB_PATH="/home/$HOME_NAME/Downloads/$EXENSION/$FILE_NAME.$EXENSION"
 
 # Função para obter a URL do último lançamento
 get_latest_release_url() {
     api_url="https://api.github.com/repos/$REPO/releases/latest"
-    deb_url=$(curl -s $api_url | grep -E 'browser_download_url.*\.deb"' | cut -d '"' -f 4)
+    # descomente a 1ª versão e comente a 2ª, instalando o jq com sudo apt install jq
+    deb_url=$(curl -s $api_url | jq -r '.assets[] | select(.name | endswith("amd64.deb")) | .browser_download_url')
+    # deb_url=$(curl -s $api_url | grep -o 'https://github.com/[^"]*\.deb')
     echo $deb_url
 }
 
@@ -26,6 +28,7 @@ download_deb() {
         exit 1
     fi
     echo "Baixando o pacote da URL: $deb_url"
+#    wget -O "$TEMP_DEB_PATH" "$deb_url"
     curl -L -o "$TEMP_DEB_PATH" "$deb_url"
 }
 
@@ -33,16 +36,15 @@ download_deb() {
 install_deb() {
     echo "Instalando o pacote .deb..."
     sudo dpkg -i "$TEMP_DEB_PATH"
-    sudo apt-get install -f -y  # Instalar dependências faltantes, se houver
+    sudo apt-get install -fy  # Instalar dependências faltantes, se houver
 }
 
 # Função para desinstalar o aplicativo
 uninstall_app() {
     echo "Desinstalando o aplicativo..."
+    # Substitua 'gmail-desktop' pelo nome real do pacote
     sudo apt-get remove --purge -y "$FILE_NAME"
-    sudo apt-get autopurge -y  # Remover dependências não mais necessárias
-    # Limpando o arquivo .deb temporário
-    rm -f "$TEMP_DEB_PATH"
+    sudo apt-get autopurge -fy  # Remover dependências não mais necessárias
 }
 
 # Checando argumentos de entrada
@@ -55,3 +57,17 @@ else
     echo "Uso: $0 {install|uninstall}"
     exit 1
 fi
+
+# Limpando o arquivo .deb temporário
+rm -f "$TEMP_DEB_PATH"
+
+# usage:
+#   => first: to make the script executable
+#   chmod +x gmail.sh
+#  => then run the script with either of the following commands
+#   ./gmail.sh install
+#   or
+#   ./gmail.sh uninstall
+
+### to verify if URL is still valid
+# curl -s https://api.github.com/repos/$REPO/releases/latest | grep "browser_download_url"
